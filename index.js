77fca40f8f11a81e041a1d21f70886c2a3c712d3
ziaochina <ziaochina@gmail.com>
2017-09-28 15:17:23 +0800

    zlj

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as data from './data'
 
 export default {
 	name: "mk-app-voucher",
-	version: "1.0.4",
+	version: "1.0.5",
 	description: "mk-app-voucher",
 	meta: data.getMeta(),
 	components: [],
